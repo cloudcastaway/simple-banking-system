16afb92335f4dc050b0874121fce05fdb9b9571f
cloudcastaway <66567923+cloudcastaway@users.noreply.github.com>
2024-12-30 22:14:00 -0300

    feat(stage-2-of-4): change card number generator to comply with the Luhn algorithm

--- src/credit_card.py
+++ src/credit_card.py
@@ -3,8 +3,7 @@ import random
 class CreditCard:
 
     @staticmethod
-    def gen_card_number():
-        card_number =  "400000" + f"{random.randint(0, 999999999):09}"
+    def calculate_checksum(card_number):
         summ = 0
         for i in range(0, len(card_number), 2):
             number = int(card_number[i]) * 2
@@ -13,10 +12,12 @@ class CreditCard:
             summ += number
         for i in range(1, len(card_number), 2):
             summ += int(card_number[i])
-        aux = summ
-        while aux % 10 != 0:
-            aux += 1
-        checksum = aux - summ
+        return (10 - (summ % 10)) % 10
+
+    @staticmethod
+    def gen_card_number():
+        card_number =  "400000" + f"{random.randint(0, 999999999):09}"
+        checksum = CreditCard.calculate_checksum(card_number)
         return card_number + str(checksum)
 
     @staticmethod
